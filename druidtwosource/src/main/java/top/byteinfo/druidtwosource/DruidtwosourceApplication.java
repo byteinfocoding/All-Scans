fc643bba09a0561b52396e9aa6f15aea8ada5a1a
byte <byteinfocoding@163.com>
2021-11-17 08:26:36 +0800

    druidtwosource created!

--- druidtwosource/src/main/java/top/byteinfo/druidtwosource/DruidtwosourceApplication.java
+++ druidtwosource/src/main/java/top/byteinfo/druidtwosource/DruidtwosourceApplication.java
@@ -2,8 +2,13 @@ package top.byteinfo.druidtwosource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+})
 public class DruidtwosourceApplication {
 
     public static void main(String[] args) {
