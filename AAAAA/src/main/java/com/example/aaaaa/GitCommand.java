0e9f7d25897c04b951a8f68156ad1fdb0b94a532
coding <byteinfocoding@yeah.net>
2022-03-04 09:50:15 +0800

    add a lot

--- AAAAA/src/main/java/com/example/aaaaa/GitCommand.java
+++ AAAAA/src/main/java/com/example/aaaaa/GitCommand.java
@@ -6,13 +6,14 @@ public class GitCommand {
      * git clone https://user/repo.git
      * failed to connect to github.com 443
      */
-    void FixProblem(){
+    void FixProblem() {
         /**
          * solutions
          * https://stackoverflow.com/questions/128035/how-do-i-pull-from-a-git-repository-through-an-http-proxy
          */
         /*command*/
-       // git config --global http.proxy http://127.0.0.1:10809
+        // git config --global http.proxy http://127.0.0.1:10809
+        // git reset HEAD <file>  移除暂存区文件
 
     }
 }
---
+++ GIT/src/main/java/top/byteinfo/git/GitApplication.java
@@ -0,0 +1,14 @@
+package top.byteinfo.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+    //https://blog.whyoop.com/2017/07/10/git%E5%B8%B8%E7%94%A8%E5%91%BD%E4%BB%A4%E6%95%B4%E7%90%86/
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class, args);
+    }
+
+}
