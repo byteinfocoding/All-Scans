663e8a71385b26ee2707ba249309f1b8ff7d93d7
coding <byteinfocoding@yeah.net>
2022-03-15 09:57:05 +0800

    adds

---
+++ SpringCloud/GatewayMall/src/main/java/com/example/gatewaymall/GatewayMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewaymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMallApplication.class, args);
+    }
+
+}
---
+++ SpringCloud/GatewayMall/src/test/java/com/example/gatewaymall/GatewayMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
